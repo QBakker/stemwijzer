3011c368f0669d79a44b3336e0eba2ee03a69015
Quinten Bakker <quinten_bakker@live.nl>
2019-02-19 14:47:49 +0100

    add logo
    website logo added,
    changed amount of questions for easy acces.

--- js/script.js
+++ js/script.js
@@ -6,9 +6,8 @@ const homepage = document.getElementById('homepage');
 const questions = document.getElementById('questions');
 const results = document.getElementById('results');
 
-const options = document.getElementsByClassName('options');
-
 const question = document.getElementById('question');
+const statement = document.getElementById('statement');
 
 const last = document.getElementById('last');
 
@@ -24,9 +23,10 @@ var counter = 0;
 window.onload = function()
 {
 	startPage();
+	// result();
 }
 
-// Deze functie zorgt voor het inladen van de homepage
+// Deze functie zorgt voor het inladen van de homepage en de bijbehoordende elementen.
 function startPage()
 {
 	homepage.style.display = 'block';
@@ -37,7 +37,7 @@ function startPage()
 	start.addEventListener('click', startQuiz);
 }
 
-// Deze functie zorgt voor het inladen de het vragen scherm
+// Deze functie zorgt voor het inladen de het vragen scherm en de elementen die daar bij horen.
 function startQuiz()
 {
 
@@ -54,14 +54,15 @@ function startQuiz()
 	
 	getStatement();
 }
-// Deze functie zorgt voor de result pagina
+// Deze functie zorgt voor de result pagina en de elementen die hier bij horen.
 function result()
 {
 	homepage.style.display = 'none';
 	questions.style.display = 'none';
 	results.style.display = 'block';	
 }
-// Deze functie zorgt ervoor dat je terug door de vragen heen kan
+// Deze functie zorgt ervoor dat je terug door de vragen kan navigeren.
+// Ook word er gecheckt of het de eerste vraag is als dat zo is dan word er terug gestuurd naar de homepage
 function back()
 {
 	if (counter === 0) {
@@ -72,7 +73,10 @@ function back()
 	}
 }
 
-// Deze functie bekijkt welk antwoord ingevuld
+// Deze functie bekijkt welk antwoord ingevuld door de eventlistener uit te lezen.
+// De waarde word dan gecheckt en vervolgens in opgeslagen in de 'answers' array opgeslagen 
+// op de juiste plek door de counter value die meegegeven word.
+
 function answer(value)
 {
 	switch (value.target.id)
@@ -112,8 +116,9 @@ function getStatement()
 		result();
 	} else if (counter <= subjects.length) {
 		question.innerHTML = counter + 1 + '. ' + subjects[counter].title;
+		statement.innerHTML = subjects[counter].statement;
 	} else {
-		question.innerHTML = "Something went wrong";
+		question.innerHTML = "Something went wrong, please try again later";
 	}
 }	
 
